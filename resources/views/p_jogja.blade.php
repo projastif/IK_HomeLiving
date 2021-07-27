1ef52df1ff03dbfb6d7170abc88ab05ac4043308
projastif <projastif@gmail.com>
2021-07-27 23:52:46 +0700

    update portfolio detail#2

--- resources/views/p_jogja.blade.php
+++ resources/views/p_jogja.blade.php
@@ -1,7 +1,153 @@
 @extends('includes.master')
 
 @section('content')
+<!-- ======= About Me Section ======= -->
+<section id="about" class="about">
+	<div class="container">
+		<div class="section-title">
+			<span>Project Yogyakarta</span>
+			<h2>Project Yogyakarta</h2>
+			<!--p>Sit sint consectetur velit quisquam cupiditate impedit suscipit alias</p-->
+		</div>
 
-<h1>JOGJA</h1>
+		<div class="row" style="padding-bottom: 70px;">
+			<div class="col-lg-6 d-flex align-items-stretch justify-content-center justify-content-lg-start">
+				<img src="assets/img/detail/jogja/after/2.jpg" style="width: 550px;">
+			</div>
+			<div class="col-lg-6 d-flex align-items-stretch justify-content-center justify-content-lg-start">
+				<div class="col-lg-12 d-flex flex-column align-items-stretch">
+					<div class="content ps-lg-6 d-flex flex-column justify-content-center">
+						<div class="row" style="text-align: justify;">
+							<h2 style="text-align: center;">KAMAR TIDUR UTAMA</h2><hr>
+							<p><i><b>Contemporer Classic Style</b></i> menjadi pilihan saya untuk kamar tidur utama ini. Penggunaan warna putih yang dominan untuk menimbulkan kesan bersih dan nyaman, sementara penambahan warna coklat di nakas dan lantai untuk menimbulkan kesan serius namun lembut dan hangat.</p>
+							<br>
+							<br>
+							<p><i><b>"Color can be used to emphasize not only what is large and what is small but also what is up and what is down"</b></i> (Joseph Albers,<b><i> artist</i></b>)</p>
+							<br>
+							<br>
+							<p>Dalam pemilihan <b><i>wallpaper</i></b>, saya memilih bahan yang berkualitas baik dan kali ini <b><i>Goodrich</i></b> menjadi pilihan saya. Mutu yang baik akan menjadi investasi yang baik pula, karena tahan lama. </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row" style="padding-bottom: 100px;">
+			<div class="col-lg-4 d-flex align-items-stretch justify-content-center justify-content-lg-start">
+				<div class="col-lg-12 d-flex flex-column align-items-stretch">
+					<div class="content ps-lg-6 d-flex flex-column justify-content-center">
+						<div class="row" style="text-align: justify;">
+							<h2 style="text-align: center;">KAMAR TIDUR ANAK</h2><hr>
+							<p>Konsep kamar bayi ini adalah <i><b>"Shape is up"</b></i>. Saya bereksperimen dengan pola bergaris vertikal yang terlihat pada <b><i>baby box</i></b> dan  <b><i>wallpaper</i></b> nya. Pemilihan warna <b><i>neutral classic</i></b> putih agar terkesan lega dan bersih.</p>
+							<br>
+							<br>
+							<p><i><b>"My design philosophy is: from follow feelings"</b></i> (Hella Jongerius, <b><i>Industrial designer</i></b>)</p>
+							<br>
+							<br>
+							<p>Penambahan aksen-aksen dengan bentuk geometrik dan warna biru pada aksesoris dinding menjadi sentuhan yang lengkap pada ruangan yang kecil ini.</p>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-8 d-flex align-items-stretch justify-content-center justify-content-lg-start">
+				<img src="assets/img/detail/jogja/after/3.jpg" style="width: 750px;">
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="section-title">
+			<span>Before - After</span>
+			<h2>Before - After</h2>
+		</div>
+
+		<div class="row">
+			<div class="col-lg-6 d-flex align-items-stretch justify-content-center justify-content-lg-start" style="margin-bottom: 25px;">
+				<img src="assets/img/detail/jogja/before/1.jpg" style="width: 550px;">
+			</div>
+			<div class="col-lg-6 d-flex align-items-stretch justify-content-center justify-content-lg-start" style="margin-bottom: 25px;">
+				<img src="assets/img/detail/jogja/after/2.jpg" style="width: 550px;">
+			</div>	
+
+			<div class="col-lg-6 d-flex align-items-stretch justify-content-center justify-content-lg-start">
+				<img src="assets/img/detail/jogja/before/2.jpg" style="width: 550px;">
+			</div>
+			<div class="col-lg-6 d-flex align-items-stretch justify-content-center justify-content-lg-start">
+				<img src="assets/img/detail/jogja/after/4.jpg" style="width: 550px;">
+			</div>	
+		</div>
+		
+	</div>
+</div>
+</section><!-- End About Me Section -->
+
+
+
+
+<!-- ======= My Portfolio Section ======= -->
+<section id="portfolio" class="portfolio">
+	<div class="container">
+
+		<div class="section-title">
+			<span>Completed Projects</span>
+			<h2>Completed Projects</h2>
+			<p>Berikut beberapa proyek yang telah kami selesaikan</p>
+		</div>
+
+		<div class="row portfolio-container">
+			<div class="col-lg-4 col-md-6 portfolio-item filter-jogja">
+				<div class="portfolio-img"><img src="assets/img/portfolio/proyek/jogja/jogja1.jpg" class="img-fluid" alt=""></div>
+				<div class="portfolio-info">
+					<h4>Project Yogyakarta - Condong Catur</h4>
+					<p>Bathroom</p>
+					<a href="assets/img/portfolio/proyek/jogja/jogja1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Bathroom from Project Yogyakarta - Condong Catur"><p>View &nbsp&nbsp&nbsp</p></a>
+					<a href="portfolio-details.html" class="details-link" title=""><p>Detail</p></a>
+				</div>
+			</div>
+
+			<div class="col-lg-4 col-md-6 portfolio-item filter-jogja">
+				<div class="portfolio-img"><img src="assets/img/portfolio/proyek/jogja/jogja2.jpg" class="img-fluid" alt=""></div>
+				<div class="portfolio-info">
+					<h4>Project Yogyakarta - Condong Catur</h4>
+					<p>Bedroom</p>
+					<a href="assets/img/portfolio/proyek/jogja/jogja2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Bedroom from Project Yogyakarta - Condong Catur"><p>View &nbsp&nbsp&nbsp</p></a>
+					<a href="portfolio-details.html" class="details-link" title=""><p>Detail</p></a>
+				</div>
+			</div>
+
+			<div class="col-lg-4 col-md-6 portfolio-item filter-jogja">
+				<div class="portfolio-img"><img src="assets/img/portfolio/proyek/jogja/jogja3.jpg" class="img-fluid" alt=""></div>
+				<div class="portfolio-info">
+					<h4>Project Yogyakarta - Condong Catur</h4>
+					<p>Reading Corner</p>
+					<a href="assets/img/portfolio/proyek/jogja/jogja3.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Reading Corner from Project Yogyakarta - Condong Catur"><p>View &nbsp&nbsp&nbsp</p></a>
+					<a href="portfolio-details.html" class="details-link" title=""><p>Detail</p></a>
+				</div>
+			</div>
+
+			<div class="col-lg-4 col-md-6 portfolio-item filter-jogja">
+				<div class="portfolio-img"><img src="assets/img/portfolio/proyek/jogja/jogja4.jpg" class="img-fluid" alt=""></div>
+				<div class="portfolio-info">
+					<h4>Project Yogyakarta - Condong Catur</h4>
+					<p>Reading Corner</p>
+					<a href="assets/img/portfolio/proyek/jogja/jogja4.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Reading Corner from Project Yogyakarta - Condong Catur"><p>View &nbsp&nbsp&nbsp</p></a>
+					<a href="portfolio-details.html" class="details-link" title=""><p>Detail</p></a>
+				</div>
+			</div>
+
+			<div class="col-lg-4 col-md-6 portfolio-item filter-jogja">
+				<div class="portfolio-img"><img src="assets/img/portfolio/proyek/jogja/jogja5.jpg" class="img-fluid" alt=""></div>
+				<div class="portfolio-info">
+					<h4>Project Yogyakarta - Condong Catur</h4>
+					<p>Reading Corner</p>
+					<a href="assets/img/portfolio/proyek/jogja/jogja5.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Reading Corner from Project Yogyakarta - Condong Catur"><p>View &nbsp&nbsp&nbsp</p></a>
+					<a href="portfolio-details.html" class="details-link" title=""><p>Detail</p></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section><!-- End My Portfolio Section -->
 
 @endsection
--- resources/views/p_manado.blade.php
+++ resources/views/p_manado.blade.php
@@ -1,119 +1,196 @@
 @extends('includes.master')
 
 @section('content')
-<!-- ======= My Portfolio Section ======= -->
-    <section id="portfolio" class="portfolio">
-      <div class="container">
+<!-- ======= About Me Section ======= -->
+<section id="about" class="about">
+  <div class="container">
+    <div class="section-title">
+      <span>Project Yogyakarta</span>
+      <h2>Project Yogyakarta</h2>
+      <!--p>Sit sint consectetur velit quisquam cupiditate impedit suscipit alias</p-->
+    </div>
 
-        <div class="section-title">
-          <span>My Portfolio</span>
-          <h2>My Portfolio</h2>
-          <p>Sit sint consectetur velit quisquam cupiditate impedit suscipit alias</p>
-        </div>
-
-        <ul id="portfolio-flters" class="d-flex justify-content-center">
-          <li data-filter="*" class="filter-active">All</li>
-          <li data-filter=".filter-app">Bitung</li>
-          <li data-filter=".filter-card">Solo</li>
-          <li data-filter=".filter-web">Yogyakarta</li>
-          <li data-filter=".filter-desain">Desain</li>
-        </ul>
-
-        <div class="row portfolio-container">
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-desain">
-            <div class="portfolio-img"><img src="assets/img/portfolio/proyek/bitung1.jpg" class="img-fluid" alt=""></div>
-            <div class="portfolio-info">
-              <h4>Kamar Mandi</h4>
-              <p>Proyek Bitung</p>
-              <a href="assets/img/portfolio/proyek/bitung1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 1"><p>Open</p>&nbsp&nbsp&nbsp</a>
-              <a href="/detail_bitung1" class="details-link" title="More Details"><p>Detail</p></i></a>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-            <div class="portfolio-img"><img src="assets/img/portfolio/proyek/bitung2.jpg" class="img-fluid" alt=""></div>
-            <div class="portfolio-info">
-              <h4>Web 3</h4>
-              <p>Web</p>
-              <a href="assets/img/portfolio/proyek/bitung2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Web 3"><i class="bx bx-plus"></i></a>
-              <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-            <div class="portfolio-img"><img src="assets/img/portfolio/proyek/bitung3.jpg" class="img-fluid" alt=""></div>
-            <div class="portfolio-info">
-              <h4>App 2</h4>
-              <p>App</p>
-              <a href="assets/img/portfolio/proyek/bitung3.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 2"><i class="bx bx-plus"></i></a>
-              <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-            <div class="portfolio-img"><img src="assets/img/portfolio/proyek/bitung4.jpg" class="img-fluid" alt=""></div>
-            <div class="portfolio-info">
-              <h4>Card 2</h4>
-              <p>Card</p>
-              <a href="assets/img/portfolio/proyek/bitung4.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Card 2"><i class="bx bx-plus"></i></a>
-              <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-            <div class="portfolio-img"><img src="assets/img/portfolio/proyek/bitung5.jpg" class="img-fluid" alt=""></div>
-            <div class="portfolio-info">
-              <h4>Web 2</h4>
-              <p>Web</p>
-              <a href="assets/img/portfolio/proyek/bitung5.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Web 2"><i class="bx bx-plus"></i></a>
-              <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-            <div class="portfolio-img"><img src="assets/img/portfolio/proyek/bitung6.jpg" class="img-fluid" alt=""></div>
-            <div class="portfolio-info">
-              <h4>App 3</h4>
-              <p>App</p>
-              <a href="assets/img/portfolio/proyek/bitung6.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 3"><i class="bx bx-plus"></i></a>
-              <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-            <div class="portfolio-img"><img src="assets/img/portfolio/proyek/bitung7.jpg" class="img-fluid" alt=""></div>
-            <div class="portfolio-info">
-              <h4>Card 1</h4>
-              <p>Card</p>
-              <a href="assets/img/portfolio/proyek/bitung7.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Card 1"><i class="bx bx-plus"></i></a>
-              <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-            <div class="portfolio-img"><img src="assets/img/portfolio/proyek/bitung8.jpg" class="img-fluid" alt=""></div>
-            <div class="portfolio-info">
-              <h4>Card 3</h4>
-              <p>Card</p>
-              <a href="assets/img/portfolio/proyek/bitung8.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Card 3"><i class="bx bx-plus"></i></a>
-              <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-            <div class="portfolio-img"><img src="assets/img/portfolio/proyek/bitung9.jpg" class="img-fluid" alt=""></div>
-            <div class="portfolio-info">
-              <h4>Web 3</h4>
-              <p>Web</p>
-              <a href="assets/img/portfolio/proyek/bitung9.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Web 3"><i class="bx bx-plus"></i></a>
-              <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
-            </div>
-          </div>
-
-        </div>
-
+    <div class="row" style="padding-bottom: 70px;">
+      <div class="col-lg-6 d-flex align-items-stretch justify-content-center justify-content-lg-start">
+        <img src="assets/img/detail/jogja/after/2.jpg" style="width: 550px;">
       </div>
-    </section><!-- End My Portfolio Section -->
+      <div class="col-lg-6 d-flex align-items-stretch justify-content-center justify-content-lg-start">
+        <div class="col-lg-12 d-flex flex-column align-items-stretch">
+          <div class="content ps-lg-6 d-flex flex-column justify-content-center">
+            <div class="row" style="text-align: justify;">
+              <h2 style="text-align: center;">KAMAR TIDUR UTAMA</h2><hr>
+              <p><i><b>Contemporer Classic Style</b></i> menjadi pilihan saya untuk kamar tidur utama ini. Penggunaan warna putih yang dominan untuk menimbulkan kesan bersih dan nyaman, sementara penambahan warna coklat di nakas dan lantai untuk menimbulkan kesan serius namun lembut dan hangat.</p>
+              <br>
+              <br>
+              <p><i><b>"Color can be used to emphasize not only what is large and what is small but also what is up and what is down"</b></i> (Joseph Albers,<b><i> artist</i></b>)</p>
+              <br>
+              <br>
+              <p>Dalam pemilihan <b><i>wallpaper</i></b>, saya memilih bahan yang berkualitas baik dan kali ini <b><i>Goodrich</i></b> menjadi pilihan saya. Mutu yang baik akan menjadi investasi yang baik pula, karena tahan lama. </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row" style="padding-bottom: 100px;">
+      <div class="col-lg-4 d-flex align-items-stretch justify-content-center justify-content-lg-start">
+        <div class="col-lg-12 d-flex flex-column align-items-stretch">
+          <div class="content ps-lg-6 d-flex flex-column justify-content-center">
+            <div class="row" style="text-align: justify;">
+              <h2 style="text-align: center;">KAMAR TIDUR ANAK</h2><hr>
+              <p>Konsep kamar bayi ini adalah <i><b>"Shape is up"</b></i>. Saya bereksperimen dengan pola bergaris vertikal yang terlihat pada <b><i>baby box</i></b> dan  <b><i>wallpaper</i></b> nya. Pemilihan warna <b><i>neutral classic</i></b> putih agar terkesan lega dan bersih.</p>
+              <br>
+              <br>
+              <p><i><b>"My design philosophy is: from follow feelings"</b></i> (Hella Jongerius, <b><i>Industrial designer</i></b>)</p>
+              <br>
+              <br>
+              <p>Penambahan aksen-aksen dengan bentuk geometrik dan warna biru pada aksesoris dinding menjadi sentuhan yang lengkap pada ruangan yang kecil ini.</p>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-8 d-flex align-items-stretch justify-content-center justify-content-lg-start">
+        <img src="assets/img/detail/jogja/after/3.jpg" style="width: 750px;">
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="section-title">
+      <span>Before - After</span>
+      <h2>Before - After</h2>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-6 d-flex align-items-stretch justify-content-center justify-content-lg-start" style="margin-bottom: 25px;">
+        <img src="assets/img/detail/jogja/before/1.jpg" style="width: 550px;">
+      </div>
+      <div class="col-lg-6 d-flex align-items-stretch justify-content-center justify-content-lg-start" style="margin-bottom: 25px;">
+        <img src="assets/img/detail/jogja/after/2.jpg" style="width: 550px;">
+      </div>  
+
+      <div class="col-lg-6 d-flex align-items-stretch justify-content-center justify-content-lg-start">
+        <img src="assets/img/detail/jogja/before/2.jpg" style="width: 550px;">
+      </div>
+      <div class="col-lg-6 d-flex align-items-stretch justify-content-center justify-content-lg-start">
+        <img src="assets/img/detail/jogja/after/4.jpg" style="width: 550px;">
+      </div>  
+    </div>
+    
+  </div>
+</div>
+</section><!-- End About Me Section -->
+
+
+
+
+<!-- ======= My Portfolio Section ======= -->
+<section id="portfolio" class="portfolio">
+  <div class="container">
+
+    <div class="section-title">
+      <span>Completed Projects</span>
+      <h2>Completed Projects</h2>
+      <p>Berikut beberapa proyek yang telah kami selesaikan</p>
+    </div>
+
+    <div class="row portfolio-container">
+      <div class="col-lg-4 col-md-6 portfolio-item filter-jogja">
+        <div class="portfolio-img"><img src="assets/img/portfolio/proyek/bitung/bitung1.jpg" class="img-fluid" alt=""></div>
+        <div class="portfolio-info">
+          <h4>Project Manado - Bitung</h4>
+          <p>Bathroom</p>
+          <a href="assets/img/portfolio/proyek/bitung/bitung1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Bathroom from Project Manado - Bitung"><p>View &nbsp&nbsp&nbsp</p></a>
+          <a href="portfolio-details.html" class="details-link" title=""><p>Detail</p></a>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 portfolio-item filter-jogja">
+        <div class="portfolio-img"><img src="assets/img/portfolio/proyek/bitung/bitung2.jpg" class="img-fluid" alt=""></div>
+        <div class="portfolio-info">
+          <h4>Project Manado - Bitung</h4>
+          <p>Bedroom</p>
+          <a href="assets/img/portfolio/proyek/bitung/bitung2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Bedroom from Project Manado - Bitung"><p>View &nbsp&nbsp&nbsp</p></a>
+          <a href="portfolio-details.html" class="details-link" title=""><p>Detail</p></a>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 portfolio-item filter-jogja">
+        <div class="portfolio-img"><img src="assets/img/portfolio/proyek/bitung/bitung3.jpg" class="img-fluid" alt=""></div>
+        <div class="portfolio-info">
+          <h4>Project Manado - Bitung</h4>
+          <p>Reading Corner</p>
+          <a href="assets/img/portfolio/proyek/bitung/bitung3.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Reading Corner from Project Manado - Bitung"><p>View &nbsp&nbsp&nbsp</p></a>
+          <a href="portfolio-details.html" class="details-link" title=""><p>Detail</p></a>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 portfolio-item filter-jogja">
+        <div class="portfolio-img"><img src="assets/img/portfolio/proyek/bitung/bitung4.jpg" class="img-fluid" alt=""></div>
+        <div class="portfolio-info">
+          <h4>Project Manado - Bitung</h4>
+          <p>Reading Corner</p>
+          <a href="assets/img/portfolio/proyek/bitung/bitung4.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Reading Corner from Project Manado - Bitung"><p>View &nbsp&nbsp&nbsp</p></a>
+          <a href="portfolio-details.html" class="details-link" title=""><p>Detail</p></a>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 portfolio-item filter-jogja">
+        <div class="portfolio-img"><img src="assets/img/portfolio/proyek/bitung/bitung5.jpg" class="img-fluid" alt=""></div>
+        <div class="portfolio-info">
+          <h4>Project Manado - Bitung</h4>
+          <p>Reading Corner</p>
+          <a href="assets/img/portfolio/proyek/bitung/bitung5.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Reading Corner from Project Manado - Bitung"><p>View &nbsp&nbsp&nbsp</p></a>
+          <a href="portfolio-details.html" class="details-link" title=""><p>Detail</p></a>
+        </div>
+      </div>
+
+
+
+
+      <div class="col-lg-4 col-md-6 portfolio-item filter-jogja">
+        <div class="portfolio-img"><img src="assets/img/portfolio/proyek/bitung/bitung6.jpg" class="img-fluid" alt=""></div>
+        <div class="portfolio-info">
+          <h4>Project Manado - Bitung</h4>
+          <p>Bedroom</p>
+          <a href="assets/img/portfolio/proyek/bitung/bitung6.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Bedroom from Project Manado - Bitung"><p>View &nbsp&nbsp&nbsp</p></a>
+          <a href="portfolio-details.html" class="details-link" title=""><p>Detail</p></a>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 portfolio-item filter-jogja">
+        <div class="portfolio-img"><img src="assets/img/portfolio/proyek/bitung/bitung7.jpg" class="img-fluid" alt=""></div>
+        <div class="portfolio-info">
+          <h4>Project Manado - Bitung</h4>
+          <p>Reading Corner</p>
+          <a href="assets/img/portfolio/proyek/bitung/bitung7.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Reading Corner from Project Manado - Bitung"><p>View &nbsp&nbsp&nbsp</p></a>
+          <a href="portfolio-details.html" class="details-link" title=""><p>Detail</p></a>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 portfolio-item filter-jogja">
+        <div class="portfolio-img"><img src="assets/img/portfolio/proyek/bitung/bitung8.jpg" class="img-fluid" alt=""></div>
+        <div class="portfolio-info">
+          <h4>Project Manado - Bitung</h4>
+          <p>Reading Corner</p>
+          <a href="assets/img/portfolio/proyek/bitung/bitung8.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Reading Corner from Project Manado - Bitung"><p>View &nbsp&nbsp&nbsp</p></a>
+          <a href="portfolio-details.html" class="details-link" title=""><p>Detail</p></a>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 portfolio-item filter-jogja">
+        <div class="portfolio-img"><img src="assets/img/portfolio/proyek/bitung/bitung9.jpg" class="img-fluid" alt=""></div>
+        <div class="portfolio-info">
+          <h4>Project Manado - Bitung</h4>
+          <p>Reading Corner</p>
+          <a href="assets/img/portfolio/proyek/bitung/bitung9.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Reading Corner from Project Manado - Bitung"><p>View &nbsp&nbsp&nbsp</p></a>
+          <a href="portfolio-details.html" class="details-link" title=""><p>Detail</p></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section><!-- End My Portfolio Section -->
 
 @endsection
