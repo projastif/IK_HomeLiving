7034053537d4daf1195cd9ff51fc5f708348ed8e
projastif <projastif@gmail.com>
2021-07-29 22:37:22 +0700

    update portfolio detail#5

--- resources/views/p_jakarta.blade.php
+++ resources/views/p_jakarta.blade.php
@@ -3,13 +3,13 @@
 @section('content')
 
 <!-- ======= Hero Section ======= -->
-  <section id="hero">
-    <div class="hero-container">
-      <h1>Project</h1>
-      <h2>Jakarta - Pinang Ranti</h2>
-      <a href="#about" class="btn-scroll scrollto" title="Scroll Down"><i class="bx bx-chevron-down"></i></a>
-    </div>
-  </section><!-- End Hero -->
+<section id="hero">
+  <div class="hero-container">
+    <h1>Project</h1>
+    <h2>Jakarta - Pinang Ranti</h2>
+    <a href="#about" class="btn-scroll scrollto" title="Scroll Down"><i class="bx bx-chevron-down"></i></a>
+  </div>
+</section><!-- End Hero -->
 
 <!-- ======= About Me Section ======= -->
 <section id="about" class="about">
@@ -21,25 +21,22 @@
     </div>
 
     <div class="row" style="padding-bottom: 70px;">
-      <div class="col-lg-6 d-flex align-items-stretch justify-content-center justify-content-lg-start">
-        <img src="assets/img/detail/Jakarta/2.jpg" style="width: 550px;">
+      <div class="col-lg-7 d-flex align-items-stretch justify-content-center justify-content-lg-start">
+        <img src="assets/img/detail/Jakarta/11.png" style="padding-bottom: 10px">
       </div>
-      <div class="col-lg-6 d-flex align-items-stretch justify-content-center justify-content-lg-start">
+      <div class="col-lg-5 d-flex align-items-stretch justify-content-center justify-content-lg-start">
         <div class="col-lg-12 d-flex flex-column align-items-stretch">
           <div class="content ps-lg-6 d-flex flex-column justify-content-center">
             <div class="row" style="text-align: justify;">
               <h2 style="text-align: center;">RUANG TAMU</h2><hr>
-              <p>Mengusung konsep antik dan modern dengan <b><i>classic style</i></b> maskulin dalam proyek ini menjadi pilihan saya. Sang pemilik rumah sangat menyukai ornament dan simbol-simbol budaya jawa sehingga tampilan kokoh dan tahan lama menjadi acuan saya untuk menghadirkan nuansa yang diinginkan.</p>
+              <p>Perencanaan konsep harus menitikberatkan latar belakang dan fungsi utama sebuah rumah yaitu rumah dengan <b><i>existing</i></b> eksterior yang sudah ada dan dengan karakter penghuni rumah sehingga tujuan dalam mendesain dapat tercapai. Saat mengerjakan proyek ini, saya mengedepankan kenyamannan penghuninya.</p>
               <br>
               <br>
-              <p><i><b>"Good design means that anything which is good by it self will go with other things"</b></i> (Jean Risom, <b><i>Industrial designer</i></b>)</p>
-              <br>
-              <br>
-              <p>Mengkombinasikan unsur antik seperti wayang dan ukiran 3D batik di dinding dengan unsur seperti modern sofa <b><i>orange</i></b> dan meja bundar kecil menciptakan ruang lebih harmonis dan tidak terkesan penuh atau sesak. Pemilihan material yang didominasi kayu jati Blora dengan <b><i>finishing coating</i></b> PU berwarna coklat seperti pada panel dinding, menimbulkan kesan kokoh dan maskulin.</p>
+              <p><b><i>Open space</i></b> konsep menjadi pilihan saya dengan menggunakan <b><i>style</i></b> modern klasik <b><i>utilize natural light, "Light is the element which arouses sheer fascination"</i></b> (Andree Putman, <b><i>Interior designer</i></b>). Banyak perubahan sipil dalam proyek ini agar ruangan terlihat lebih luas dan sirkulasi udara dan cahaya terpenuhi dengan memanfaatkan sisi jendela sepanjang ruang tamu dan ruang keluarga. Bertema modern dengan sentuhan klasik ringan pada ruangan ini dapat terlihat meja bundar yang bertatakan marmer hitam dipadu dengan <b><i>stainless steel</i></b> emas yang sangat berkarakter.</p>
             </div>
           </div>
         </div>
-      </div>
+      </div>    
     </div>
 
     <div class="row" style="padding-bottom: 70px;">
@@ -47,39 +44,35 @@
         <div class="col-lg-12 d-flex flex-column align-items-stretch">
           <div class="content ps-lg-6 d-flex flex-column justify-content-center">
             <div class="row" style="text-align: justify;">
-              <h2 style="text-align: center;">RUANG KELUARGA</h2><hr>
-              <p><b><i>"Dare to be different"</i></b></p>
+              <h2 style="text-align: center;">RUANG MAKAN</h2><hr>
+              <p>Konsep ruang makan ini adalah kontemporer klasik dengan material kayu pada kursi dan meja makannya. Sedangkan <b><i>upholtry</i></b> kursi makannya dipilih menggunakan warna biru tosca dengan bahan velvet yang tebal dikombinasikan dengan tali krem di pinggir kursi untuk menambah kontras yang kalem.</p>
               <br>
               <br>
-              <p><i><b>"Design is one of the very few fields beda what difference are celebrated"</b></i>(Yves Behar, <i><b>Industrial designer</b></i>)</p>
+              <p><b><i>"To do much more with much less"</i></b> (George Nelson, <b><i>Industrial designer</i></b>).</p>
               <br>
               <br>
-              <p>Dalam penataan ruangan tidak ada hukum baku yang mengharuskan seluruh furnitur seragam dan berwarna sama. Padu padan furnitur berbeda warna sah-sah saja. Apa yang diperhatikan adalah keharmonisan dan keseimbangan ruangan secara keseluruhan. </p>
-              <br>
-              <br>
-              <p>Pemilihan aksesoris yang tepat akan membuat ruangan terlihat lebih menarik. Aksesoris dengan nuansa daerah tertentu dapat menjadi fokus tersendiri pada ruangan ini.</p>
+              <p>Sebuah fokus sangat dibutuhkan salam penataan ruang jenis apapun, nah.. lampu gantung bundar yang tergantung  di atas meja terbuat dari <b><i>stainless steel</i></b> emas yang membuat terlihat elegan dan terkesan kokoh merupakan pilihan saya untuk menyeimbangkan meja bundar klasik yang berwarna coklat tua.</p>
             </div>
           </div>
         </div>
       </div>
       <div class="col-lg-8 d-flex align-items-stretch justify-content-center justify-content-lg-start">
-        <img src="assets/img/detail/Jakarta/5.jpg" style="width: 750px;">
+        <img src="assets/img/detail/Jakarta/2.png" style="width: 750px;">
       </div>
     </div>
 
-    <div class="row" style="padding-bottom: 70px;">
-      <div class="col-lg-9 d-flex align-items-stretch justify-content-center justify-content-lg-start">
-        <img src="assets/img/detail/Jakarta/3.jpg" style="width: 730px;">
+    <div class="row">
+      <div class="col-lg-7 d-flex align-items-stretch justify-content-center justify-content-lg-start">
+        <img src="assets/img/detail/Jakarta/3.png" style="width: 730px;">
       </div>
-      <div class="col-lg-3 d-flex align-items-stretch justify-content-center justify-content-lg-start">
+      <div class="col-lg-5 d-flex align-items-stretch justify-content-center justify-content-lg-start">
         <div class="col-lg-12 d-flex flex-column align-items-stretch">
           <div class="content ps-lg-6 d-flex flex-column justify-content-center">
             <div class="row" style="text-align: justify;">
-              <h2 style="text-align: center;">KAMAR TIDUR UTAMA</h2><hr>
-              <p>Posisi tempat tidur yang bersandar pada dinding panel yang tinggi membuat kamar utama ini tampak lebih besar, <b><i>get the position right</i></b> dengan memutuskan <b><i>layout</i></b> yang tepat, maka ruang yang tampak kecil bisa terlihat lebih lega.</p>
-              <br>
-              <br>
-              <p><b><i>"The quality of design is important to the well-being of humanity"</i></b> (Robin Bay, <i><b>Designer</b></i>)</p>
+              <h2 style="text-align: center;">KAMAR TIDUR ANAK</h2><hr>
+              <p>Dalam penataan ruang tidur ini, furnitur yang dipakai cukup lengkap yaitu meliputi: Tempat tidur 160cm x 200cm, nakas, meja belajar, dan lemari pakaian <b><i>built in</i></b>.</p>
+              <br><br>
+              <p>Semua diatur pada satu sisi bidang karena ruang kamarnya yang sangat sempit. Sementara tempat tidur diletakkan pada sis jendela supaya mendapatkan cukup cahaya matahari.</p>
             </div>
           </div>
         </div>
@@ -87,7 +80,7 @@
     </div>
   </div>
 
-  <div class="container">
+  <!--div class="container">
     <div class="section-title">
       <span>Before - After</span>
       <h2>Before - After</h2>
@@ -95,21 +88,20 @@
 
     <div class="row">
       <div class="col-lg-6 d-flex align-items-stretch justify-content-center justify-content-lg-start" style="margin-bottom: 25px;">
-        <img src="assets/img/detail/Jakarta/2.jpg" style="width: 550px;">
+        <img src="assets/img/detail/Jakarta/2.png" style="width: 550px;">
       </div>
       <div class="col-lg-6 d-flex align-items-stretch justify-content-center justify-content-lg-start" style="margin-bottom: 25px;">
-        <img src="assets/img/detail/Jakarta/3.jpg" style="width: 550px;">
+        <img src="assets/img/detail/Jakarta/3.png" style="width: 550px;">
       </div>  
 
       <div class="col-lg-6 d-flex align-items-stretch justify-content-center justify-content-lg-start">
-        <img src="assets/img/detail/Jakarta/2b.jpg" style="width: 550px;">
+        <img src="assets/img/detail/Jakarta/2b.png" style="width: 550px;">
       </div>
       <div class="col-lg-6 d-flex align-items-stretch justify-content-center justify-content-lg-start">
-        <img src="assets/img/detail/Jakarta/3b.jpg" style="width: 550px;">
+        <img src="assets/img/detail/Jakarta/3b.png" style="width: 550px;">
       </div>  
     </div>
-    
-  </div>
+  </div-->
 </div>
 </section><!-- End About Me Section -->
 
@@ -129,79 +121,52 @@
         <div class="portfolio-info">
           <h4>Project Jakarta - Pinang Ranti</h4>
           <p>Bathroom</p>
-          <a href="assets/img/detail/Jakarta/1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Bathroom from Project Jakarta - Pinang Ranti"><p>View &nbsp&nbsp&nbsp</p></a>
+          <a href="assets/img/detail/Jakarta/1.png" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Bathroom from Project Jakarta - Pinang Ranti"><p>View &nbsp&nbsp&nbsp</p></a>
         </div>
       </div>
 
       <div class="col-lg-4 col-md-6 portfolio-item filter-jogja">
-        <div class="portfolio-img"><img src="assets/img/detail/Jakarta/2.jpg" class="img-fluid" alt=""></div>
+        <div class="portfolio-img"><img src="assets/img/detail/Jakarta/6.png" class="img-fluid" alt=""></div>
         <div class="portfolio-info">
           <h4>Project Jakarta - Pinang Ranti</h4>
           <p>Bedroom</p>
-          <a href="assets/img/detail/Jakarta/2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Bedroom from Project Jakarta - Pinang Ranti"><p>View &nbsp&nbsp&nbsp</p></a>
+          <a href="assets/img/detail/Jakarta/2.png" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Bedroom from Project Jakarta - Pinang Ranti"><p>View &nbsp&nbsp&nbsp</p></a>
         </div>
       </div>
 
       <div class="col-lg-4 col-md-6 portfolio-item filter-jogja">
-        <div class="portfolio-img"><img src="assets/img/detail/Jakarta/3.jpg" class="img-fluid" alt=""></div>
+        <div class="portfolio-img"><img src="assets/img/detail/Jakarta/3.png" class="img-fluid" alt=""></div>
         <div class="portfolio-info">
           <h4>Project Jakarta - Pinang Ranti</h4>
           <p>Reading Corner</p>
-          <a href="assets/img/detail/Jakarta/3.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Reading Corner from Project Jakarta - Pinang Ranti"><p>View &nbsp&nbsp&nbsp</p></a>
+          <a href="assets/img/detail/Jakarta/3.png" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Reading Corner from Project Jakarta - Pinang Ranti"><p>View &nbsp&nbsp&nbsp</p></a>
         </div>
       </div>
 
       <div class="col-lg-4 col-md-6 portfolio-item filter-jogja">
-        <div class="portfolio-img"><img src="assets/img/detail/Jakarta/4.jpg" class="img-fluid" alt=""></div>
+        <div class="portfolio-img"><img src="assets/img/detail/Jakarta/4.png" class="img-fluid" alt=""></div>
         <div class="portfolio-info">
           <h4>Project Jakarta - Pinang Ranti</h4>
           <p>Reading Corner</p>
-          <a href="assets/img/detail/Jakarta/4.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Reading Corner from Project Jakarta - Pinang Ranti"><p>View &nbsp&nbsp&nbsp</p></a>
+          <a href="assets/img/detail/Jakarta/4.png" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Reading Corner from Project Jakarta - Pinang Ranti"><p>View &nbsp&nbsp&nbsp</p></a>
         </div>
       </div>
 
       <div class="col-lg-4 col-md-6 portfolio-item filter-jogja">
-        <div class="portfolio-img"><img src="assets/img/detail/Jakarta/5.jpg" class="img-fluid" alt=""></div>
-        <div class="portfolio-info">
-          <h4>Project Jakarta - Pinang Ranti</h4>
-          <p>Reading Corner</p>
-          <a href="assets/img/detail/Jakarta/5.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Reading Corner from Project Jakarta - Pinang Ranti"><p>View &nbsp&nbsp&nbsp</p></a>
-        </div>
-      </div>
-
-      <div class="col-lg-4 col-md-6 portfolio-item filter-jogja">
-        <div class="portfolio-img"><img src="assets/img/detail/Jakarta/6.jpg" class="img-fluid" alt=""></div>
+        <div class="portfolio-img"><img src="assets/img/detail/Jakarta/6.png" class="img-fluid" alt=""></div>
         <div class="portfolio-info">
           <h4>Project Jakarta - Pinang Ranti</h4>
           <p>Bedroom</p>
-          <a href="assets/img/detail/Jakarta/6.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Bedroom from Project Jakarta - Pinang Ranti"><p>View &nbsp&nbsp&nbsp</p></a>
+          <a href="assets/img/detail/Jakarta/6.png" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Bedroom from Project Jakarta - Pinang Ranti"><p>View &nbsp&nbsp&nbsp</p></a>
         </div>
       </div>
 
       <div class="col-lg-4 col-md-6 portfolio-item filter-jogja">
-        <div class="portfolio-img"><img src="assets/img/detail/Jakarta/10.jpg" class="img-fluid" alt=""></div>
+        <div class="portfolio-img"><img src="assets/img/detail/Jakarta/9.png" class="img-fluid" alt=""></div>
         <div class="portfolio-info">
           <h4>Project Jakarta - Pinang Ranti</h4>
           <p>Reading Corner</p>
-          <a href="assets/img/detail/Jakarta/9.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Reading Corner from Project Jakarta - Pinang Ranti"><p>View &nbsp&nbsp&nbsp</p></a>
-        </div>
-      </div>
-
-      <div class="col-lg-4 col-md-6 portfolio-item filter-jogja">
-        <div class="portfolio-img"><img src="assets/img/detail/Jakarta/9.jpg" class="img-fluid" alt=""></div>
-        <div class="portfolio-info">
-          <h4>Project Jakarta - Pinang Ranti</h4>
-          <p>Reading Corner</p>
-          <a href="assets/img/detail/Jakarta/7.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Reading Corner from Project Jakarta - Pinang Ranti"><p>View &nbsp&nbsp&nbsp</p></a>
-        </div>
-      </div>
-
-      <div class="col-lg-4 col-md-6 portfolio-item filter-jogja">
-        <div class="portfolio-img"><img src="assets/img/detail/Jakarta/8.jpg" class="img-fluid" alt=""></div>
-        <div class="portfolio-info">
-          <h4>Project Jakarta - Pinang Ranti</h4>
-          <p>Reading Corner</p>
-          <a href="assets/img/detail/Jakarta/8.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Reading Corner from Project Jakarta - Pinang Ranti"><p>View &nbsp&nbsp&nbsp</p></a>
+          <a href="assets/img/detail/Jakarta/7.png" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Reading Corner from Project Jakarta - Pinang Ranti"><p>View &nbsp&nbsp&nbsp</p></a>
         </div>
       </div>
     </div>
